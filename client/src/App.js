4c3bbee12cee609aa56cfa2bb4e6067fc758fdd9
josephine0900 <76456547+josephine0900@users.noreply.github.com>
2022-06-29 16:18:46 -0400

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import { Routes, Route } from "react-router-dom";
+import React, {useState} from 'react';
+import './App.css';
+import './Progress.css';
 
-import Layout from "./components/Layout";
-import Login from "./components/Login";
-import Home from "./components/Home";
-import ProtectedRoutes from "./auth/ProtectedRoutes";
-import MajorProgress from "./components/MajorProgress";
 
-function App() {
+const Progress = ({done}) => {
   return (
-    // Setting up routes and elements
-    <Routes>
-      {/* Wrapping each page around Layout element */}
-      <Route element={<Layout />}>
-        {/* Login route at index */}
-        <Route path="/" element={<Login />} />
-        {/* Protected Routes */}
-        <Route element={<ProtectedRoutes />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/progress" element={<MajorProgress />} />
-        </Route>
-      </Route>
-    </Routes>
+    <div className="progress">
+      <div class="progress-done" style={{ opacity: 1, width: `${done}%`}}></div>
+    </div>
+  )
+}
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="container my-5">
+      <div className="card text-center my-5">
+        <div className="card-body">
+          <h1>Progress Bar</h1>
+          <div className="my-5">
+            <Progress done={count}/>
+            <h2 className='my-5'>Completed: {count}%</h2>
+            <button className='btn btn-success mx-3' onClick={() => setCount(count + 1)}>Increment</button>
+            <button className='btn btn-danger mx-3' onClick={() => setCount(count - 1)} disabled={count === 0}>Decrement</button>
+            <button className='btn btn-secondary mx-3' onClick={() => setCount(0)} disabled={count === 0}>Reset</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
